667029c0b2e5c26c6653f57b03db991d8f89c98e
TomerKL9 <tomerklg@gmail.com>
2024-07-17 17:25:15 +0300

    Delete BBB_side directory

--- BBB_side/gpio.h
+++
@@ -1,9 +0,0 @@
-#ifndef GPIO_H
-#define GPIO_H
-
-void gpio_export(int pin);
-void gpio_unexport(int pin);
-void gpio_set_direction(int pin, const char *direction);
-int gpio_get_value(int pin);
-
-#endif // GPIO_H
